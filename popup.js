258e38adfdd163e79803e29172e2a0b919ad5737
Shreyas Gupta <sgupta17@kentdenver.org>
2015-12-02 09:45:58 -0700

    Notification & Icon

--- popup.js
+++ popup.js
@@ -1,5 +1,16 @@
-var sched;
-$.get("https://spark.kentdenver.org/schedule_widget/", function(data) {
-  sched = data;
-});
-console.log(sched);
+// var sched = "";
+// $.get("https://spark.kentdenver.org/schedule_widget/", function(data) {
+//   sched = data;
+//   console.log(sched);
+//   var newDoc = $(data);
+//   var dayLabel = newDoc.find('#daylabel');
+//   console.log(dayLabel);
+// });
+
+// $(document).ready( function() {
+// var iframe = $("#frame");
+//   iframe = $("#frame");
+//   console.log(iframe);
+//   $('iframe').contents().find('#daylabel').css('color','red');
+//   // $('iframe').contents().find('#header').css('opacity','.2');
+// });
